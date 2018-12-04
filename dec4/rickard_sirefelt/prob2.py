8ac650b4fc2d105410b1b7965d0cad247b47456b
Rickard Sirefelt <rickard.sirefelt@smarteye.se>
2018-12-04 09:21:16 +0100

    dec4 rickard sirefelt

--- dec4/rickard_sirefelt/prob2.py
+++ dec4/rickard_sirefelt/prob2.py
@@ -13,8 +13,6 @@ schedule.sort(key=lambda change: change["datetime"])
 
 guard_sleep_time = {}
 guard_on_shift = 0
-sleepiest_guard = 0
-max_sleep = 0
 start_sleep, end_sleep = 0, 0
 for change in schedule:
     if change["event"][0] == "G":
@@ -27,16 +25,19 @@ for change in schedule:
         end_sleep = change["datetime"].minute
         sleep_array[start_sleep:end_sleep] += 1
         if guard_on_shift not in guard_sleep_time:
-            guard_sleep_time[guard_on_shift] = [
-                sum(sleep_array), sleep_array
-            ]
+            guard_sleep_time[guard_on_shift] = sleep_array
         else:
-            guard_sleep_time[guard_on_shift][0] += sum(sleep_array)
-            guard_sleep_time[guard_on_shift][1] = np.vstack(
-                [guard_sleep_time[guard_on_shift][1], sleep_array])
-            if guard_sleep_time[guard_on_shift][0] > max_sleep:
-                max_sleep = guard_sleep_time[guard_on_shift][0]
-                sleepiest_guard = guard_on_shift
-sleep_per_min = sum(guard_sleep_time[sleepiest_guard][1])
-sleepiest_min = np.argmax(sleep_per_min)
-print(sleepiest_guard * sleepiest_min)
+            guard_sleep_time[guard_on_shift] = np.vstack(
+                [guard_sleep_time[guard_on_shift], sleep_array])
+max_sleepiest_min = 0
+max_sleep_in_max_sleepiest_min = 0
+sleepiest_guard = 0
+for guard, guard_sleep in guard_sleep_time.items():
+    tot_sleep_each_min = sum(guard_sleep)
+    sleepiest_min = np.argmax(tot_sleep_each_min)
+    sleep_in_sleepiest_min = tot_sleep_each_min[sleepiest_min]
+    if sleep_in_sleepiest_min > max_sleep_in_max_sleepiest_min:
+        max_sleep_in_max_sleepiest_min = sleep_in_sleepiest_min
+        max_sleepiest_min = sleepiest_min
+        sleepiest_guard = guard
+print(sleepiest_guard * max_sleepiest_min)
