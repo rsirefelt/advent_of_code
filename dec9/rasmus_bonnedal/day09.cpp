e39e44a938970f292175bdea265c97442d946e8e
Rasmus Bonnedal <rasmus.bonnedal@gmail.com>
2018-12-09 16:47:50 +0100

    Add Rasmus day 9 part 2 solution

--- dec9/rasmus_bonnedal/day09.cpp
+++ dec9/rasmus_bonnedal/day09.cpp
@@ -1,46 +1,98 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <memory>
+#include <list>
 #include <numeric>
+#include <regex>
 #include <vector>
+
+template<class Base>
+class circular_iterator : public Base {
+public:
+    circular_iterator(Base begin, Base end) :
+        Base(begin)
+        ,_begin(begin)
+        , _end(end) {
+    }
+
+    void set(Base begin, Base end, Base current) {
+        _begin = begin;
+        _end = end;
+        Base::operator=(current);
+    }
+
+    circular_iterator& operator++(void) {
+        if (_begin == _end) {
+            return *this;
+        }
+        if (*this == _end) {
+            Base::operator=(_begin);
+        }
+        Base::operator++();
+        if (*this == _end) {
+            Base::operator=(_begin);
+        }
+        return *this;
+    }
+
+    circular_iterator& operator--(void) {
+        if (*this == _begin) {
+            Base::operator=(_end);
+        }
+        Base::operator--();
+        return *this;
+    }
+
+    bool operator==(Base& it) {
+        return Base::operator==(it);
+    }
+
+private:
+    Base _begin, _end;
+};
 
 class MarbleCircle {
 public:
+    MarbleCircle() :
+        _marbles()
+        , _current(_marbles.begin(), _marbles.end()) {
+    }
+
     int place(int marble) {
         int score = 0;
-        if (marble == 0) {
-            _marbles.push_back(marble);
-            _current = 0;
-        } else if (marble == 1) {
-            _marbles.push_back(marble);
-            _current = 1;
-        } else if (marble % 23 == 0) {
-            int i = index(-7);
-            score = marble + _marbles.at(i);
-            _marbles.erase(_marbles.begin() + i);
-            _current = i % _marbles.size();
-        } else {
-            _current = _marbles.insert(_marbles.begin() + index(1) + 1, marble) - _marbles.begin();
+        if (marble > 0 && marble % 23 == 0) {
+            for (int i = 0; i < 7; ++i) {
+                --_current;
+            }
+            score = marble + *_current;
+            auto newCurrent = _current;
+            ++newCurrent;
+            _marbles.erase(_current);
+            _current = newCurrent;
+        }
+        else {
+            ++_current;
+            decltype(_marbles)::iterator it = _current;
+            if (it != _marbles.end()) ++it;
+            it = _marbles.insert(it, marble);
+            _current.set(_marbles.begin(), _marbles.end(), it);
         }
         return score;
     }
 
     void print(std::ostream& stream) const {
-        for (size_t i = 0; i < _marbles.size(); ++i) {
-            if (i == _current) {
-                stream << " (" << _marbles[i] << ") ";
-            } else {
-                stream << "  " << _marbles[i] << "  ";
+        for (auto it = _marbles.begin(); it != _marbles.end(); ++it) {
+            if (it == _current) {
+                stream << " (" << *it << ") ";
+            }
+            else {
+                stream << "  " << *it << "  ";
             }
         }
     }
 private:
-    size_t index(size_t offset) {
-        return (_current + offset + _marbles.size()) % _marbles.size();
-    }
-
-    std::vector<int> _marbles;
-    size_t _current;
+    std::list<int> _marbles;
+    circular_iterator<std::list<int>::iterator> _current;
 };
 
 std::ostream& operator<<(std::ostream& stream, const MarbleCircle& mc) {
@@ -48,25 +100,38 @@ std::ostream& operator<<(std::ostream& stream, const MarbleCircle& mc) {
     return stream;
 }
 
-int problemA(int nPlayers, int nMaxMarble) {
+int64_t problemAB(int nPlayers, int nMaxMarble) {
     MarbleCircle mc;
     mc.place(0);
-    std::vector<int> players(nPlayers);
+    std::vector<int64_t> players(nPlayers);
 
     size_t curPlayer = 0;
     for (int i = 1; i <= nMaxMarble; ++i) {
         players[curPlayer] += mc.place(i);
-//        std::cout << "[" << (curPlayer + 1) << "] " << mc << std::endl;
+        //std::cout << "[" << curPlayer << "] " << mc << std::endl;
         curPlayer = (curPlayer + 1) % nPlayers;
-        if (i % 10000 == 0) {
-            std::cout << "marble: " << i << std::endl;
-        }
     }
     return *std::max_element(players.begin(), players.end());
 }
 
 int main(int, char**) {
-    std::cout << problemA(438, 71626) << std::endl;
-    std::cout << problemA(438, 7162600) << std::endl;
+    std::ifstream input("../input.txt");
+    if (!input.is_open()) {
+        std::cout << "Could not open file" << std::endl;
+        return 1;
+    }
+    std::string s;
+    std::getline(input, s);
+    std::regex re("(\\d+) players; last marble is worth (\\d+) points");
+    std::smatch m;
+    if (!std::regex_search(s, m, re) || m.size() != 3) {
+        std::cout << "Error parsing input" << std::endl;
+        return 1;
+    }
+    int nPlayers = std::stoi(m[1]);
+    int nMaxMarble = std::stoi(m[2]);
+    
+    std::cout << problemAB(nPlayers, nMaxMarble) << std::endl;
+    std::cout << problemAB(nPlayers, nMaxMarble * 100) << std::endl;
     return 0;
 }
