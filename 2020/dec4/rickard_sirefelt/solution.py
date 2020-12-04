2ba7647bf2d0b6df0a96a77483922c99e40daefc
Rickard <rickard.sirefelt@smarteye.se>
2020-12-04 12:17:13 +0100

    blu

--- 2020/dec4/rickard_sirefelt/solution.py
+++ 2020/dec4/rickard_sirefelt/solution.py
@@ -1,8 +1,6 @@
 import re
 
 req_pass_set = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-req_ecl_set = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-
 
 def pass_check1(pass_entry):
     return req_pass_set <= pass_entry.keys()
@@ -23,15 +21,13 @@ def pass_check2(pass_entry):
             re.match(r"^\d{4}$", pass_entry["eyr"])
             and 2020 <= int(pass_entry["eyr"]) <= 2030
         )
-
         hgt = False
         if re.match(r"^\d{3}cm$", pass_entry["hgt"]):
             hgt = 150 <= int(pass_entry["hgt"][:-2]) <= 193
         elif re.match(r"^\d{2}in$", pass_entry["hgt"]):
             hgt = 59 <= int(pass_entry["hgt"][:-2]) <= 76
-
         hcl = re.match(r"^#[a-f0-9]{6}$", pass_entry["hcl"])
-        ecl = pass_entry["ecl"] in req_ecl_set
+        ecl = pass_entry["ecl"] in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
         pid = re.match(r"^\d{9}$", pass_entry["pid"])
 
         valid = byr and iyr and eyr and hgt and hcl and ecl and pid
