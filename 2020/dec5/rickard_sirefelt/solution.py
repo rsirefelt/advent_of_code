bdbcaaeda32c6f5517b5abe776420fe05ed72a9f
Rickard <rickard.sirefelt@smarteye.se>
2020-12-05 10:05:30 +0100

    babboush

--- 2020/dec5/rickard_sirefelt/solution.py
+++ 2020/dec5/rickard_sirefelt/solution.py
@@ -1,10 +1,7 @@
 with open("input.txt", "r") as f:
     seats = f.read().translate(str.maketrans({"B": "1", "F": "0", "R": "1", "L": "0"}))
 
-occ_seats = set()
-for s in seats.split("\n"):
-    occ_seats.add(int(s, 2))
-
+occ_seats = set(int(s, 2) for s in seats.split("\n"))
 occ_seats_l = list(occ_seats)
 all_seats = {x for x in range(occ_seats_l[0], occ_seats_l[-1] + 1)}
 
