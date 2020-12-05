d0eda68d9aecb92a4ec4cc664e5b9a158033fb01
Rickard <rickard.sirefelt@smarteye.se>
2020-12-05 15:31:04 +0100

    ko

--- 2020/dec5/rickard_sirefelt/solution.py
+++ 2020/dec5/rickard_sirefelt/solution.py
@@ -1,9 +1,6 @@
 with open("input.txt", "r") as f:
     seats = f.read().translate(str.maketrans({"B": "1", "F": "0", "R": "1", "L": "0"}))
-
 occ_seats = set(int(s, 2) for s in seats.split("\n"))
-occ_seats_l = list(occ_seats)
-all_seats = set(x for x in range(occ_seats_l[0], occ_seats_l[-1] + 1))
-
-print(f"1) Maximum seat id: {occ_seats_l[-1]}")
+all_seats = set(x for x in range(list(occ_seats)[0], list(occ_seats)[-1] + 1))
+print(f"1) Maximum seat id: {list(occ_seats)[-1]}")
 print(f"2) Correct seat id: {(all_seats - occ_seats).pop()}")
