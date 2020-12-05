071897bca8cafcf5fc6ffa835d130216c94f74ea
Rickard <rickard.sirefelt@smarteye.se>
2020-12-05 09:55:50 +0100

    Hellu

--- 2020/dec5/rickard_sirefelt/solution.py
+++ 2020/dec5/rickard_sirefelt/solution.py
@@ -1,19 +1,14 @@
 with open("input.txt", "r") as f:
     seats = f.read().translate(str.maketrans({"B": "1", "F": "0", "R": "1", "L": "0"}))
 
-min_id, max_id = 2 ** 10, 0
 occ_seats = set()
 for s in seats.split("\n"):
     s_id = int(s, 2)
     occ_seats.add(s_id)
 
-    if s_id > max_id:
-        max_id = s_id
-    if s_id < min_id:
-        min_id = s_id
-
-all_seats = {x for x in range(min_id, max_id + 1)}
+occ_seats_l = list(occ_seats)
+all_seats = {x for x in range(occ_seats_l[0], occ_seats_l[-1] + 1)}
 correct_seat = all_seats - occ_seats
 
-print(f"1) Maximum seat id: {max_id}")
+print(f"1) Maximum seat id: {occ_seats_l[-1]}")
 print(f"2) Correct seat id: {correct_seat.pop()}")
