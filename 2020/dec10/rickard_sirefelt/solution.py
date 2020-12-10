2d97d7ad6d08844acadb1458bd92fad241d6e6f8
Rickard <rickard.sirefelt@smarteye.se>
2020-12-10 22:11:59 +0100

    fixup

--- 2020/dec10/rickard_sirefelt/solution.py
+++ 2020/dec10/rickard_sirefelt/solution.py
@@ -9,6 +9,20 @@ for i in range(1, len(input_)):
     diffs_dist[input_[i] - input_[i - 1] - 1] += 1
 
 # Part 2:
+comb_map = {0: 1}
+
+
+def consec_1_diff_comb_map(n_consec_1_diff):
+    if n_consec_1_diff not in comb_map and n_consec_1_diff > -1:
+        # Add number of combination to previous 3 adapters together which are the possible new
+        # path to the new node
+        comb_map[n_consec_1_diff] = 0
+        for i in range(1, 4):
+            comb_map[n_consec_1_diff] += consec_1_diff_comb_map(n_consec_1_diff - i)
+
+    return comb_map[n_consec_1_diff] if n_consec_1_diff > -1 else 0
+
+
 n_consec_1_diff = []
 consec_1_diff_count = 1 if input_[0] == 1 else 0
 for i in range(1, len(input_)):
@@ -19,31 +33,8 @@ for i in range(1, len(input_)):
         consec_1_diff_count = 0
 
 n_consec_1_diff.append(consec_1_diff_count)  # Last three diff
-consec_1_diff_comb_map = {0: 1, 1: 1, 2: 2, 3: 4, 4: 7}
-consec_1_comb = list(map(lambda x: consec_1_diff_comb_map[x], n_consec_1_diff))
+consec_1_comb = list(map(lambda x: consec_1_diff_comb_map(x), n_consec_1_diff))
 tot_adapter_comb = np.prod(consec_1_comb)
 
 print(f"1) 1 jolt diffs times 3 jolt diffs: {diffs_dist[0] * diffs_dist[2]}")
 print(f"2) Number of adapter combinations: {tot_adapter_comb}")
-
-"""
-consec_1_diff_comb_map:
-# Two consecutive ones
-4 5 6
-4 6
-
-# Three consecutive ones
-4 5 6 7
-4 5 7
-4 6 7
-4 7
-
-# Four consecutive ones
-4 5 6 7 8
-4 5 6 8
-4 5 7 8
-4 5 8
-4 6 7 8
-4 6 8
-4 7 8
-"""
