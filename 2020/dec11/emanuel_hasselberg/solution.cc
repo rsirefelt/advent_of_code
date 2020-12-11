764e4ef592aad4dee233bdccd4d5f6db7ab5becf
Emanuel Hasselberg <Emanuel.hasselberg@gmail.com>
2020-12-11 21:16:42 +0100

    Add solution for emanuels dec 11

--- 2020/dec11/emanuel_hasselberg/solution.cc
+++ 2020/dec11/emanuel_hasselberg/solution.cc
@@ -78,7 +78,77 @@ public:
 
   bool noOccupiedNeighbours(int i, int j)
   {
-    
+    size_t max_col = m_theMap[i].size();
+    size_t max_row = m_theMap.size();
+    for (size_t i_2 = -1; i_2 <= 1; i_2++)
+    {
+      for (size_t j_2 = -1; j_2 <= 1; j_2++)
+      {
+        if (m_theMap[max((size_t)0, min(i + i_2, max_row))][max((size_t)0, min(j + j_2, max_row))] == Place::used)
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  int nbrOfNeighbours(int i, int j)
+  {
+    int max_col = m_theMap[i].size();
+    int max_row = m_theMap.size();
+    int nbrOfUsed = 0;
+    for (int i_2 = -1; i_2 <= 1; i_2++)
+    {
+      for (int j_2 = -1; j_2 <= 1; j_2++)
+      {
+        if ((i_2 == 0 && j_2 == 0) || i + i_2 < 0 || i + i_2 > max_row - 1 ||
+            j + j_2 < 0 || j + j_2 > max_col - 1)
+        {
+          continue;
+        }
+        if (m_theMap[i + i_2][j + j_2] == Place::used)
+        {
+          nbrOfUsed++;
+        }
+      }
+    }
+    return nbrOfUsed;
+  }
+
+  int nbrOfNeighboursFar(int i, int j)
+  {
+    int max_col = m_theMap[i].size();
+    int max_row = m_theMap.size();
+    int nbrOfUsed = 0;
+    vector<pair<int, int>> directions = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+    for (auto dir : directions)
+    {
+      int len = 1;
+
+      while (true)
+      {
+        int dx = dir.first * len;
+        int dy = dir.second * len;
+
+        if (i + dx < 0 || i + dx > max_row - 1 ||
+            j + dy < 0 || j + dy > max_col - 1)
+        {
+          break;
+        }
+        if (m_theMap[i + dx][j + dy] == Place::used)
+        {
+          nbrOfUsed++;
+          break;
+        }
+        if (m_theMap[i + dx][j + dy] == Place::empty)
+        {
+          break;
+        }
+        len++;
+      }
+    }
+    return nbrOfUsed;
   }
 
   bool propagate()
@@ -89,16 +159,42 @@ public:
     {
       newMap.push_back(vector<Place>());
 
-      for (size_t j = 0; i < m_theMap[i].size(); i++)
+      for (size_t j = 0; j < m_theMap[i].size(); j++)
       {
-        if (isEmpty(i,j) && noOccupiedNeighbours(i,j))
+        // if (isEmpty(i, j) && nbrOfNeighbours(i, j) == 0)
+        if (isEmpty(i, j) && nbrOfNeighboursFar(i, j) == 0)
         {
-          change =true;
+          change = true;
           newMap[i].push_back(Place::used);
+        }
+        // else if (m_theMap[i][j] == Place::used && nbrOfNeighbours(i, j) >= 4)
+        else if (m_theMap[i][j] == Place::used && nbrOfNeighboursFar(i, j) >= 5)
+        {
+          change = true;
+          newMap[i].push_back(Place::empty);
+        }
+        else
+        {
+          newMap[i].push_back(m_theMap[i][j]);
         }
       }
     }
+    m_theMap = newMap;
     return change;
+  }
+
+  int seatsOccupied()
+  {
+    int nbrOfSeats = 0;
+    for (auto row : m_theMap)
+    {
+      for (auto tPlace : row)
+      {
+        if (tPlace == Place::used)
+          nbrOfSeats++;
+      }
+    }
+    return nbrOfSeats;
   }
 };
 
@@ -111,4 +207,5 @@ int main()
   {
     cm.print();
   }
+  cout << "Seats occupied: " << cm.seatsOccupied();
 }
