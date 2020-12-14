c10008172487254e4cc44198bf3c89eb56ce99f6
Rasmus Bonnedal <rasmus.bonnedal@gmail.com>
2020-12-14 06:47:05 +0100

    Rasmus: cleanup

---
+++ 2020/dec13/rasmus_bonnedal/ctl.hh
@@ -0,0 +1,243 @@
+#pragma once
+/*
+Christmas Template Library
+*/
+
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+
+// Return the keys of the map as a vector
+template<typename T1, typename T2>
+std::vector<T1> map_keys(const std::map<T1, T2>& m) {
+    std::vector<T1> keys;
+    keys.reserve(m.size());
+    for(const auto& kv : m) {
+        keys.push_back(kv.first);
+    }
+    return keys;
+}
+
+template<typename K, typename V>
+std::map<K, V> make_map(const std::vector<std::pair<K, V>>& vec) {
+    return std::map<K, V>(vec.begin(), vec.end());
+}
+
+template<typename T>
+std::pair<T, T> make_pair(const std::vector<T>& vec) {
+    return std::make_pair(vec[0], vec[1]);
+}
+
+template<typename T, typename Tout = typename T::iterator::value_type>
+std::set<Tout> make_set(const T& vec) {
+    return std::set<Tout>(vec.begin(), vec.end());
+}
+
+// Return true if b is a subset of a
+template<typename T>
+bool is_subset(const std::set<T>& a, const std::set<T>& b) {
+    return std::includes(a.begin(), a.end(), b.begin(), b.end());
+}
+
+// Return true if b is a subset of a
+template<typename T>
+bool is_subset(const std::vector<T>& a, const std::set<T>& b) {
+    std::set<T> a_s(a.begin(), a.end());
+    return is_subset(a_s, b);
+}
+
+template<typename T>
+bool in(const T& t, const std::set<T>& s) {
+    return s.find(t) != s.end();
+}
+
+template<typename Tvec, typename T = typename Tvec::value_type::value_type>
+std::set<T> set_union(const Tvec& sets) {
+    std::set<T> retval;
+    for (const auto& vec: sets) {
+        retval.insert(vec.begin(), vec.end());
+    }
+    return retval;
+}
+
+template<typename T>
+std::set<T> intersection(const std::vector<std::set<T>>& sets) {
+    std::set<T> retval;
+    if (sets.size() > 0) {
+        retval.insert(sets[0].begin(), sets[0].end());
+        for (auto& s: sets) {
+            std::set<T> new_set;
+            std::set_intersection(
+                s.begin(), s.end(),
+                retval.begin(), retval.end(),
+                std::inserter(new_set, new_set.begin()));
+            retval = std::move(new_set);
+        }
+    }
+    return retval;
+}
+
+// Return true if v is in range [min_v, max_v]
+bool in_range(int min_v, int v, int max_v) {
+    return min_v <= v && v <= max_v;
+}
+
+// Return true if str ends with needle
+bool ends_with(const std::string& str, const std::string& needle) {
+    if (needle.size() < str.size()) {
+        return std::equal(needle.rbegin(), needle.rend(), str.rbegin());
+    }
+    return false;
+}
+
+std::string slice(const std::string& str, size_t start, int len = 0) {
+    size_t slen;
+    if (len > 0) {
+        slen = len;
+    } else if (len == 0) {
+        slen = std::string::npos;
+    } else {
+        slen = str.length() + len;
+    }
+    return str.substr(start, slen);
+}
+
+bool is_hex(char c) {
+    return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f');
+}
+
+std::string read_file(const std::string& filename) {
+    std::ifstream ifs(filename);
+    if (ifs.fail()) {
+        throw std::runtime_error("Could not open file");
+    }
+
+    std::string str;
+    ifs.seekg(0, std::ios::end);   
+    str.reserve(ifs.tellg());
+    ifs.seekg(0, std::ios::beg);
+
+    str.assign((std::istreambuf_iterator<char>(ifs)),
+                std::istreambuf_iterator<char>());
+    return str;
+}
+
+std::vector<std::string> split(const std::string& str, const std::string& delim) {
+    std::vector<std::string> strvec;
+    size_t pos = 0;
+    int n_str = str.size();
+    while(pos < n_str) {
+        size_t end_pos = str.find(delim, pos);
+        if (end_pos == std::string::npos) {
+            strvec.push_back(str.substr(pos));
+            break;
+        }
+        if (end_pos > pos) {
+            strvec.push_back(str.substr(pos, end_pos - pos));
+        }
+        pos = end_pos + delim.length();
+    };
+    return strvec;
+}
+
+std::string replace(const std::string& str, char before, char after) {
+    std::string retval = str;
+    std::replace(retval.begin(), retval.end(), before, after);
+    return retval;
+}
+
+std::string strip(const std::string& str) {
+    std::string whites(" \t\f\v\n\r");
+    std::size_t l = str.find_first_not_of(whites);
+    std::size_t r = str.find_last_not_of(whites);
+    if (r != std::string::npos) {
+        return str.substr(l, r - l + 1);
+    } else {
+        return "";
+    }
+}
+
+long stol(const std::string& s) {
+    return std::stol(s);
+}
+
+template<typename Tvecin,
+    typename F,
+    typename T = typename Tvecin::iterator::value_type,
+    typename Tout = typename std::result_of<F(T)>::type>
+std::vector<Tout> map(F func, const Tvecin& values) {
+    std::vector<Tout> retval;
+    retval.reserve(values.size());
+    for (const auto& value: values) {
+        retval.push_back(func(value));
+    }
+    return retval;
+}
+
+template<typename T, typename F>
+std::vector<T> filter(const std::vector<T>& vec, F pred) {
+    std::vector<T> retval;
+    std::copy_if(vec.begin(), vec.end(), std::back_inserter(retval), pred);
+    return retval;
+}
+
+template<typename T>
+std::vector<T> slice(const std::vector<T>& vec, size_t start, size_t end = 0) {
+    if (end == 0) end = vec.size();
+    return std::vector<T>(vec.begin() + start, vec.begin() + end);
+}
+
+template<typename T>
+T max(const std::vector<T>& vec) {
+    return *std::max_element(vec.begin(), vec.end());
+}
+
+template<typename T>
+T min(const std::vector<T>& vec) {
+    return *std::min_element(vec.begin(), vec.end());
+}
+
+template<typename T>
+T sum(const std::vector<T>& vec) {
+    return std::accumulate(vec.begin(), vec.end(), T(0));
+}
+
+template<typename T>
+T prod(const std::vector<T>& vec) {
+    return std::accumulate(vec.begin(), vec.end(), T(1), std::multiplies<>());
+}
+
+template<typename T>
+void sort(std::vector<T>& vec) {
+    std::sort(vec.begin(), vec.end());
+}
+
+template<typename T>
+std::vector<T> sorted(const std::vector<T>& vec) {
+    auto retval = vec;
+    std::sort(retval.begin(), retval.end());
+    return retval;
+}
+
+template<typename Tvec, typename T = typename Tvec::value_type>
+size_t count(const Tvec& vec, const T& value) {
+    return std::count(vec.begin(), vec.end(), value);
+}
+
+int gcd(int a, int b) { 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b); 
+} 
+  
+int lcm(const std::vector<int>& vec) { 
+    int ans = vec[0]; 
+    for (int i = 1; i < vec.size(); i++) {
+        ans = vec[i] * ans / gcd(vec[i], ans); 
+    }
+      return ans; 
+}
--- 2020/dec13/rasmus_bonnedal/dec13.cc
+++ 2020/dec13/rasmus_bonnedal/dec13.cc
@@ -13,16 +13,6 @@ indata_t parse(const std::string& filename) {
     auto x = split(read_file(filename), "\n");
     indata_t retval;
     retval.early = std::stoi(x[0]);
-    std::vector<std::string> y = filter(split(x[1], ","),
-        [](const auto& s) { return s != "x"; });
-    retval.sched = map(stol, y);
-    return retval;
-}
-
-indata_t parse2(const std::string& filename) {
-    auto x = split(read_file(filename), "\n");
-    indata_t retval;
-    retval.early = std::stoi(x[0]);
     retval.sched = map([](const std::string& s) -> long {
         if (s == "x") return -1;
         return (long)std::stoi(s);
@@ -34,16 +24,13 @@ int check_bus(const indata_t& ind) {
     int bestbus = 0;
     int besttime = 0;
 
-
-//    int t = 11;
-//    int b = 3;
-//    leave at 12;
-
     for (const auto& s : ind.sched) {
-        int t = (ind.early / s + 1) * s;
-        if (besttime == 0 || t < besttime) {
-            bestbus = s;
-            besttime = t;
+        if (s > 0) {
+            int t = (ind.early / s + 1) * s;
+            if (besttime == 0 || t < besttime) {
+                bestbus = s;
+                besttime = t;
+            }
         }
     }
     return (besttime - ind.early) * bestbus;
@@ -55,47 +42,70 @@ int t() {
     return 1000000 * tv.tv_sec + tv.tv_usec;
 }
 
+std::vector<int> find_largest_group(const std::vector<std::pair<int64_t, int64_t>>& prs, int& group) {
+    std::map<int, int> occurrences;
+    int max = 0;
+    for (int try_group = 0; try_group < 1000; ++try_group) {
+        int members = 0;
+        for (auto &x: prs) {
+            int sched = x.first;
+            int offset = x.second;
+            if ((try_group - offset % sched) % sched == 0) {
+                members++;
+            }
+        }
+        if (members > max) {
+            std::cout << "Found offset " << try_group << " with " << members << " buses" << std::endl;
+            max = members;
+            group = try_group;
+        }
+    }
+    std::vector<int> retval;
+    for (auto &x: prs) {
+        if ((group - x.second % x.first) % x.first == 0) {
+            retval.push_back(x.first);
+        }
+    }
+    return retval;
+}
+
 int64_t check_bus2(const indata_t& ind) {
     std::vector<std::pair<int64_t, int64_t>> prs;
     for (size_t i = 0; i < ind.sched.size(); ++i) {
-        if (ind.sched[i] > 0) prs.push_back(std::make_pair<int64_t, int64_t>((int64_t)ind.sched[i], (int64_t)i));
+        if (ind.sched[i] > 0) {
+            prs.push_back(std::make_pair<int64_t, int64_t>((int64_t)ind.sched[i], (int64_t)i));
+        }
     }
-//    sort(prs);
-    for (auto& x: prs) {
-        x.second = x.second % x.first;
-    }
-/*
-    LCM(19, 41, 449) = 349771
-    349771 - 17 = 349754
-    349771 * 17 = 5946107
-    At least 100 000 000 000 000
-    56313132
-*/
 
-    int64_t t = 349771 * 17;
+    int offset;
+    int64_t t_step = lcm(find_largest_group(prs, offset));
+    std::cout << "LCM of busses in group " << offset << " is " << t_step << std::endl;
     int64_t n = 1;
     while(true) {
         bool found = true;
+        int64_t time = t_step * n - offset;
         for (auto& x: prs) {
-            if (((t*n-17) + x.second) % x.first != 0) {
+            if ((time + x.second) % x.first != 0) {
                 found = false;
                 break;
             }
         }
-        if (found) return t * n - 17;
+        if (found) {
+            std::cout << "Found after " << n << " iterations." << std::endl;
+            return time;
+        }
         n++;
     }
-
     return 0;
 }
 
-
-
 int main(int argc, char** argv) {
     auto indata = parse("input");
-    std::cout << "Part 1: " << check_bus(indata) << std::endl;
-    auto indata2 = parse2("input");
-    std::cout << "Part 2: " << check_bus2(indata2) << std::endl;
+    int part1 = check_bus(indata);
+    assert(part1 == 3035);
+    std::cout << "Part 1: " << part1 << std::endl;
+    int64_t part2 = check_bus2(indata);
+    std::cout << "Part 2: " << part2 << std::endl;
 
     return 0;
 }
