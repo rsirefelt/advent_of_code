e0d69d1fbb4ce437b69af6f5bf000b67437a68cb
Jesper Ericsson <jesper.ericsson@zenseact.com>
2021-12-01 06:25:38 +0100

    Add Jesper's first task

--- 2021/dec01/jesper_ericsson/main.py
+++ 2021/dec01/jesper_ericsson/main.py
@@ -1,4 +1,23 @@
+import numpy as np
+
+def prob1(data):
+    diff = np.diff(data)
+    negative = diff > 0
+    print(np.sum(negative))
+
+def prob2(data):
+    sliding_sums = np.convolve(data,np.ones(3,dtype=int),'valid')
+    diff = np.diff(sliding_sums)
+    negative = diff > 0
+    print(np.sum(negative))
+
 def main():
+    input_data = np.genfromtxt('testdata.csv', delimiter=',')
+    input_data = np.genfromtxt('data.csv', delimiter=',')
+
+    prob1(input_data)
+    prob2(input_data)
+
 
             
 
