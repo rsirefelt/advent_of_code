259fc852dc1c2611209a9005bc14e5da96704021
Jesper Ericsson <jesper.ericsson@zenseact.com>
2021-12-12 10:33:07 +0100

    Add Jesper's solutin day 12

--- 2021/dec12/jesper_ericsson/main.py
+++ 2021/dec12/jesper_ericsson/main.py
@@ -1,6 +1,58 @@
+import numpy as np
+
+
+def readData():
+    filename = "testdata.csv"
+    filename = "data.csv"
+    with open(filename, "r") as f:
+        input_lines = f.readlines()
+        connections = {}
+        for line in input_lines:
+            first, second = line.rstrip().split("-")
+
+            if first in connections:
+                connections[first].append(second)
+            else:
+                connections[first] = [second]
+
+            if second in connections:
+                connections[second].append(first)
+            else:
+                connections[second] = [first]
+
+    return connections
+
+
+def get_valid_paths(connections, cave, small_visited_caves, second_visited):
+    if cave == "end":
+        return 1
+
+    if cave in small_visited_caves:
+        if second_visited or cave == "start":
+            return 0
+        else:
+            second_visited = True
+
+    if cave.islower():
+        small_visited_caves.add(cave)
+    num_valid_paths = 0
+    for child in connections[cave]:
+        num_valid_paths += get_valid_paths(
+            connections, child, small_visited_caves.copy(), second_visited
+        )
+
+    return num_valid_paths
+
+
 def main():
+    connections = readData()
 
-            
+    valid_paths = get_valid_paths(connections, "start", set([]), True)
+    print("Prob1 number of valid paths:", valid_paths)
 
-if __name__ == "__main__": main()
-	
+    valid_paths2 = get_valid_paths(connections, "start", set([]), False)
+    print("Prob2 number of valid paths:", valid_paths2)
+
+
+if __name__ == "__main__":
+    main()
