6731d9a04e460d9979b17e550008bcf3e97b93dc
Jesper Ericsson <jesper.ericsson@zenseact.com>
2022-12-01 08:18:22 +0100

    Add Jesper's solution for first day
    This commit adds Jesper's solution for the first day 2022.

--- 2022/dec01/jesper_ericsson/main.py
+++ 2022/dec01/jesper_ericsson/main.py
@@ -1,20 +1,27 @@
 import numpy as np
 
 
-def prob1(data):
-    pass
-
-
-def prob2(data):
-    pass
+def prob1(filename):
+    calorie_list = []
+    with open(filename, "r") as f:
+        data_lines = f.readlines()
+        sum = 0
+        for string in data_lines:
+            if string.rstrip() != "":
+                sum += int(string.rstrip())
+            else:
+                calorie_list.append(sum)
+                sum = 0
+        calorie_list.append(sum)
+        print(f"First problem: {np.max(calorie_list)}")
+        print(f"Second problem: {np.sum(sorted(calorie_list)[-3:])}")
 
 
 def main():
-    input_data = np.genfromtxt("testdata.csv", delimiter=",")
-    input_data = np.genfromtxt("data.csv", delimiter=",")
+    filename = "testdata.csv"
+    filename = "data.csv"
 
-    prob1(input_data)
-    prob2(input_data)
+    prob1(filename)
 
 
 if __name__ == "__main__":
