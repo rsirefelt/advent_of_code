49d0ce7252c663fbb1763fb42cac99c7ea75d335
Jesper Ericsson <jesperericsson@outlook.com>
2022-12-11 22:19:47 +0100

    Add Jespers first part day 10

--- 2022/dec10/jesper_ericsson/main.py
+++ 2022/dec10/jesper_ericsson/main.py
@@ -1,20 +1,63 @@
+import os
 import numpy as np
 
+def check_and_add(x, cycle):
+    if (cycle - 20) % 40 == 0:
+        print(f"x2: {x}, cycle: {cycle}, signal: {x * cycle}")
+        return x *cycle
+    
+    return 0
 
-def prob1(data):
-    pass
+def prob1(instructions):
+    cycle = 0
+    x = 1
+    sum_signals = 0
+    for instruction in instructions:
+        # print(instruction.rstrip())
+        if instruction.rstrip() == "noop":
+            cycle += 1
+            sum_signals += check_and_add(x, cycle)
+
+        else:
+            cycle += 1
+            sum_signals += check_and_add(x, cycle)
+            cycle += 1
+            sum_signals += check_and_add(x, cycle)
+
+            x += int(instruction.rstrip()[5:])
+    print(f"Signal sum: {sum_signals}")
 
 
-def prob2(data):
-    pass
+
+def prob2(instructions):
+    position = 0
+    x = 1
+    sum_signals = 0
+    for instruction in instructions:
+
+        if instruction.rstrip() == "noop":
+            cycle += 1
+            sum_signals += check_and_add(x, cycle)
+
+        else:
+            cycle += 1
+            sum_signals += check_and_add(x, cycle)
+            cycle += 1
+            sum_signals += check_and_add(x, cycle)
+
+            x += int(instruction.rstrip()[5:])
+    print(f"Signal sum: {sum_signals}")
 
 
 def main():
-    input_data = np.genfromtxt("testdata.csv", delimiter=",")
-    input_data = np.genfromtxt("data.csv", delimiter=",")
+    dir = os.path.dirname(__file__)
+    filename = dir + "/testdata.csv"
+    # filename = dir + "/data.csv"
+    with open(filename, "r") as f:
+        instructions = f.readlines()
 
-    prob1(input_data)
-    prob2(input_data)
+    prob1(instructions)
+    # prob2(map)
 
 
 if __name__ == "__main__":
